cd7b5a82f4651b1d9faeac1ff5c418eedbad9bc6
Will Schroeder <MY_NAME@example.com>
2020-02-02 14:01:42 -0800

    hash

---
+++ typescript/hash_table.ts
@@ -0,0 +1,103 @@
+type HashTable = {
+    a: Array<Array<HashValue>>
+    size: number
+}
+
+type HashValue = {
+    key: string 
+    value: string 
+}
+
+function print(val: any) {
+    console.log(val)
+}
+
+// https://github.com/darkskyapp/string-hash/blob/master/index.js
+function hashStr(str: string) {
+    var hash = 5381 
+    var i = str.length
+
+    while(i) {
+      hash = (hash * 33) ^ str.charCodeAt(--i)
+    }
+  
+    return hash >>> 0
+}
+
+function slotForKey(key: string, size: number): number {
+    return hashStr(key) % size
+}
+
+function findHashValueIndex(a: Array<HashValue>, key: string): number {
+    if (a.length == 0) {
+        return -1 
+    }
+
+    let index = -1
+    for (let i = 0; i < a.length; i++) {
+        if (a[i].key == key) {
+            index = i 
+            break 
+        }
+    }
+
+    return index
+}
+
+function set(hash: HashTable, key: string, value: string) {
+    let slotIndex = slotForKey(key, hash.size)
+    let slotListIndex = findHashValueIndex(hash.a[slotIndex], key)
+
+    if (slotListIndex > -1) {
+        hash.a[slotIndex][slotListIndex].value = value 
+    }
+    else {
+        hash.a[slotIndex].push({key: key, value: value})
+    }
+}
+
+function get(hash: HashTable, key: string): string|null {
+    let slotIndex = slotForKey(key, hash.size)
+    let slotListIndex = findHashValueIndex(hash.a[slotIndex], key)
+
+    if (slotListIndex > -1) {
+        return hash.a[slotIndex][slotListIndex].value
+    }
+
+    return null 
+} 
+
+function del(hash: HashTable, key: string) {
+    let slotIndex = slotForKey(key, hash.size)
+    let slotListIndex = findHashValueIndex(hash.a[slotIndex], key)
+
+    if (slotListIndex > -1) {
+        hash.a[slotIndex].splice(slotListIndex, 1)
+    }
+}
+
+function hashTableConstructor(size: number): HashTable {
+    const hash: HashTable = {a: [], size: size}
+    for(let i = 0; i < size; i++) {
+        hash.a[i] = []
+    }
+    return hash 
+}
+
+print ("Hash Table")
+const hash = hashTableConstructor(10)
+set(hash, "foo", "bar")
+print(get(hash, "foo"))
+set(hash, "foo", "other")
+print(get(hash, "foo"))
+del(hash, "foo")
+print(get(hash, "foo"))
+
+print ("Hash Table One Slot")
+const smallHash = hashTableConstructor(1)
+set(smallHash, "foo", "bar")
+set(smallHash, "test", "lol")
+print(get(smallHash, "foo"))
+print(get(smallHash, "test"))
+del(smallHash, "foo")
+print(smallHash.a)
