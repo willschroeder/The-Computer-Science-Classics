35f7ae0df4f7d5c88e83bb082585c6f462085540
Will Schroeder <MY_NAME@example.com>
2020-02-03 22:53:59 -0800

    topological sort

--- graph.ts
+++ graph.ts
@@ -1,5 +1,5 @@
 type Vertex = {
-    value : number 
+    value : any 
     index: number 
 }
 
@@ -17,7 +17,7 @@ function print(val: any) {
     console.log(val)
 }
 
-function addVertex(graph: Graph, value: number): Vertex {
+function addVertex(graph: Graph, value: any): Vertex {
     let index = graph.verts.length
     let vert = {value: value, index: index}
     graph.verts.push({value: value, index: index})
@@ -29,7 +29,7 @@ function addDirectedEdge(graph: Graph, from: Vertex, to: Vertex) {
     graph.edges[from.index].push({to: to, from: from})
 }
 
-function breadthFirstSearch(graph: Graph, value: number): boolean {
+function breadthFirstSearch(graph: Graph, value: any): boolean {
     let toSearch: Array<Vertex> = [graph.verts[0]]
     let serached = {}
 
@@ -51,7 +51,7 @@ function breadthFirstSearch(graph: Graph, value: number): boolean {
     return false 
 }
 
-function findPath(graph: Graph, vert: Vertex, value: number, pathSoFar: Array<number>): Array<number>|null {
+function findPath(graph: Graph, vert: Vertex, value: any, pathSoFar: Array<number>): Array<number>|null {
     let pathToNode = pathSoFar.concat(vert.index)
 
     if (vert.value == value) {
@@ -68,6 +68,53 @@ function findPath(graph: Graph, vert: Vertex, value: number, pathSoFar: Array<nu
     }
 
     return null 
+}
+
+function topologicalSort(graph: Graph): Array<Vertex> {
+    enum Status {
+        Unprocessed,
+        Processing,
+        Processed
+    }
+
+    let visited: Array<Status> = new Array(graph.verts.length).fill(Status.Unprocessed)
+    let order: Array<Vertex> = []
+    
+    for(let i = 0; i < graph.verts.length; i++) {
+        if (visited[i] == Status.Processed) {
+            continue 
+        }
+        if (visited[i] == Status.Processing) {
+            throw `vertex ${i} cycle detected, root`
+        }
+
+        let toProcess: Array<Vertex> = [graph.verts[i]]
+        let processed: Array<Vertex> = []
+        while(toProcess.length > 0) {
+            let node = toProcess.shift()
+            visited[node.index] = Status.Processing 
+            processed.push(node)
+
+            graph.edges[node.index].forEach((edge) => {
+                let nodeToVisit = edge.to 
+                let nodeToVisitStatus = visited[nodeToVisit.index]
+                if (nodeToVisitStatus == Status.Unprocessed) {
+                    toProcess.push(nodeToVisit)
+                }
+                else if (nodeToVisitStatus == Status.Processing) {
+                    throw `vertex ${nodeToVisit.index} cycle detected, edge lookup`
+                }
+            })
+        }
+
+        order = order.concat(processed)
+
+        processed.forEach((vert) => {
+            visited[vert.index] = Status.Processed
+        })
+    }
+
+    return order 
 }
 
 /*
@@ -104,4 +151,21 @@ print(breadthFirstSearch(graph, 22))
 print(findPath(graph, v[0], 5, []))
 print(findPath(graph, v[0], 55, []))
 
+print(topologicalSort(graph).map((i) => { return i.value }))
 
+
+const dag: Graph = {verts: [], edges: []}
+
+//                 0   1   2   3   4   5
+let letterList = ['a','b','c','d','e','f']
+let v2 = letterList.map((i) => {
+    return addVertex(dag, i)
+})
+
+addDirectedEdge(dag, v2[0], v2[3])
+addDirectedEdge(dag, v2[5], v2[1])
+addDirectedEdge(dag, v2[1], v2[3])
+addDirectedEdge(dag, v2[5], v2[0])
+addDirectedEdge(dag, v2[3], v2[2])
+
+print(topologicalSort(dag).map((i) => { return i.value }))
