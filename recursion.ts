a7bf9da40e6a189f7035d7ef744815301a3aee17
Will Schroeder <MY_NAME@example.com>
2020-02-04 21:47:07 -0800

    stair jump memo

--- recursion.ts
+++ recursion.ts
@@ -1,5 +1,8 @@
 function stairJumper(jumpsTaken: Array<number>, stairsLeft: number, permutations: Array<Array<number>>) {
-    if (stairsLeft <= 0) {
+    if (stairsLeft < 0) {
+        return 
+    }
+    if (stairsLeft == 0) {
         permutations.push(jumpsTaken)
         return 
     }
@@ -16,5 +19,31 @@ function stairJumper(jumpsTaken: Array<number>, stairsLeft: number, permutations
 }
 
 let perms = []
-stairJumper([], 3, perms)
-console.log(perms)
+stairJumper([], 10, perms)
+console.log(perms.length)
+
+function stairJumperMemo(jumpsTaken: number, stairsLeft: number, memo: {[stairsLeft: number]: number}): number {
+    if (stairsLeft == 0) {
+        return 1
+    }
+    if (stairsLeft < 0) {
+        return 0 
+    }
+
+    let count = 0
+    for (let i = 1; i < 3+1; i++) {
+        if (memo[stairsLeft-i]) {
+            count += memo[stairsLeft-i] 
+        }
+        else {
+            let got = stairJumperMemo(jumpsTaken + i, stairsLeft-i, memo)
+            // console.log(`memo ${stairsLeft-i}: ${got}`)
+            memo[stairsLeft-i] = got 
+            count += got  
+        }
+    }
+
+    return count 
+}
+
+console.log(stairJumperMemo(0, 10, {}))
